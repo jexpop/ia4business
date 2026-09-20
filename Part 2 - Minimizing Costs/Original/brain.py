52859959bd3c7a992985d008a0774d92ea113fd1
jexpop <46821108+jexpop@users.noreply.github.com>
2026-09-20 16:09:28 +0200

    min costes

--- Part 2 - Minimizing Costs/Original/brain.py
+++ Part 2 - Minimizing Costs/Original/brain.py
@@ -15,5 +15,5 @@ class Brain(object):
         x = Dense(units = 64, activation = "sigmoid")(states)
         y = Dense(units = 32, activation = "sigmoid")(x)
         q_values = Dense(units = number_actions, activation = "softmax")(y)
-        self.model = Model(inputs = states, outputs = q_values)
-        self.model.compile(loss = "mse", optimizer = Adam(learning_rate = learning_rate))
+        self.model = Model(inputs = states, output = q_values)
+        self.model.compile(loss = "mse", optimizer = Adam(lr = learning_rate))
